c8d2d867a5bca2d6e5575c0730dc9d7f7f7e3495
Theop101005 <dexter2404@icloud.com>
2023-03-25 18:51:17 +0000

    button fronts

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from sys import exit
+pygame.font.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400
+font = pygame.font.SysFont("freesansbold.ttf", 18)
 
 pygame.init
 window_icon = pygame.image.load("blood-cells.png")
@@ -14,26 +16,43 @@ mainMenu = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Grimcell")
 # naming the surface
 
+FPS = 60
 clock = pygame.time.Clock()
 # creating a clock to base frame rate off of
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill("gold4")
-
 mouse = pygame.mouse.get_pos()
 
-pygame.draw.rect(mainMenu, "black", [590, 315, 80 , 30])
+class Button:
+    def __init__(self, text, x_pos, y_pos, width, height, screen):
+        self.text = text
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.draw()
 
-selectImg = pygame.load()
+    def draw(self):
+        button_text = font.render(self.text, True, "black")
+        button_rect = pygame.rect.Rect((self.x_pos, self.y_pos),(self.width ,self.height))
+        pygame.draw.rect(self.screen, "gray", button_rect, 0, 5)
+        pygame.draw.rect(self.screen, "black", button_rect, 2, 5)
+        mainMenu.blit(button_text, (self.x_pos + 10, self.y_pos + 10))
 
 while True:
+    mainMenu.fill("white")
+    clock.tick(FPS)
+    level_select_btn = Button("Level Select", 350, 100, 100, 30, mainMenu)
+    level_create_btn = Button("Level Create", 350, 150, 100, 30, mainMenu)
+    endless_btn = Button("Endless", 350, 200, 100, 30, mainMenu)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    mainMenu.blit(test_surface,(0,0))
-
+    pygame.display.flip()
     pygame.display.update()
-    clock.tick(60)
+   
     # This is to cap the frame rate of the game and stop any issues that would occur through high frame rates
